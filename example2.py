a35f5ad410f966aa963e9063ec1e33e1b48cef7c
bharath <bharathbhat87@gmail.com>
2026-08-27 00:26:38 +0530

    inital phyton study

---
+++ example2.py
@@ -0,0 +1,5 @@
+name=input("Enter your name: ")
+print("welcome", name)
+inp=input("europe floor area: ")
+usa=int(inp)+1
+print("USA floor area:", usa)
---
+++ hello.py
@@ -0,0 +1,23 @@
+hours=35.0
+rate=12.50
+pay=hours*rate
+print("pay:", pay)
+x=0.6
+x=3.6*x+45*55
+print("x:", x)
+ddd=5+4
+print("ddd:", ddd)
+eee="hello"+"world"
+print("eee:", eee)
+type(eee)
+type(ddd)
+print("type of eee:", type(eee))
+print("type of ddd:", type(ddd))
+print(10/2)
+sval='5'
+type(sval)
+print("type of sval:", type(sval))
+ival=int(sval)
+type(ival)
+print("type of ival:", type(ival))
+print("ival+1:", ival+1)
