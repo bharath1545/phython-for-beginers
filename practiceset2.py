ca7030d8ac256e74c07335286035f31ed96886ad
bharath1545 <bharathbhat87@gmail.com>
2026-09-03 21:39:26 +0530

    new files added dd

---
+++ practiceset2.py
@@ -0,0 +1,3 @@
+hi={"chalye": "go wayard", "kya": "what", "kaise": "how", "kab": "when", "kahan": "where"}
+word=input("enter the hindi word: ")
+print(hi[word])
--- unios and intersection.py
+++ unios and intersection.py
@@ -2,5 +2,8 @@
 # The union of two sets A and B is denoted as A ∪ B, and it includes all elements that are in A, in B, or in both. Duplicate elements are automatically removed in the resulting set.
 s1={1,45,6}
 s2={7,8,1}
+#methods in sets
 print(s1.union(s2)) # Using the union() method to combine s1 and s2
 print(s1.intersection(s2)) # Using the intersection() method to find common elements between s1 and s2
+print(s1.issubset(s2)) # Using the issubset() method to check if s1 is a subset of s2
+print(s1.difference(s2)) # Using the difference() method to find elements in s1 that are not in s2
